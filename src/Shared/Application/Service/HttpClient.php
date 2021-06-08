0405d3bded937eafd18c52b5beee343ce4f23478
furbyus@gmail.com <furbyus@gmail.com>
2021-06-08 22:06:25 +0200

    Not extend from a final class Guzzle

--- src/Shared/Application/Service/HttpClient.php
+++ src/Shared/Application/Service/HttpClient.php
@@ -3,8 +3,19 @@
 namespace Paymefy\Shared\Application\Service;
 
 use GuzzleHttp\Client as GuzzleClient;
-
-class HttpClient extends GuzzleClient
+use Psr\Http\Message\ResponseInterface;
+class HttpClient
 {
+    private GuzzleClient $guzzleClient;
+
+    public function __construct()
+    {
+        $this->guzzleClient = new GuzzleClient();
+    }
+
+    public function get(string $url): ResponseInterface
+    {
+        return $this->guzzleClient->get($url);
+    }
 
 }
